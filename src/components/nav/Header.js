a80e0e0dafa0f2a67f24e321f36d0e2b28aeb918
Richard <cguo3889@uni.sydney.edu.au>
2022-05-16 04:47:55 +1000

    upload

---
+++ src/components/nav/Header.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import {Menu} from 'antd'
+import {MailOutlined, AppstoreOutlined, SettingOutlined} from '@ant-design/icons'
+
+
+const {Submenu} = Menu;
+
+const Header = () => {
+    const [state, useState] = useState('');
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Header;
